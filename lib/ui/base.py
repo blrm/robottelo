4970fe820e71ac02d40f441a47b0f6bf2c3eac66
Blair Morrison <blairmorrison@gmail.com>
2013-12-12 11:26:51 -0500

    reverting some hacks to see if it worked originally

--- lib/ui/base.py
+++ lib/ui/base.py
@@ -18,24 +18,24 @@ class Base():
     logger = logging.getLogger("robottelo")
     logger.setLevel(int(conf.properties['nosetests.verbosity']))
 
-    # def find_element(self, locator):
-    #     """
-    #     Wrapper around Selenium's WebDriver that allows you to search for an
-    #     element in the web page.
-    #     """
-    #
-    #     try:
-    #         _webelement = self.browser.find_element(*locator)
-    #         if _webelement.is_displayed():
-    #             return _webelement
-    #         else:
-    #             return None
-    #     except NoSuchElementException, e:
-    #         self.logger.debug("Could not locate element '%s'." % locator[1])
-    #         return None
-    #     except Exception, e:
-    #         self.logger.debug("Failed to locate element. ERROR: %s" % str(e))
-    #         return None
+    def find_element(self, locator):
+        """
+        Wrapper around Selenium's WebDriver that allows you to search for an
+        element in the web page.
+        """
+
+        try:
+            _webelement = self.browser.find_element(*locator)
+            if _webelement.is_displayed():
+                return _webelement
+            else:
+                return None
+        except NoSuchElementException, e:
+            self.logger.debug("Could not locate element '%s'." % locator[1])
+            return None
+        except Exception, e:
+            self.logger.debug("Failed to locate element. ERROR: %s" % str(e))
+            return None
 
     def wait_until_element(self, locator, delay=20):
 
@@ -43,9 +43,22 @@ class Base():
         Wrapper around Selenium's WebDriver that allows you to pause your test
         until an element in the web page is present.
         """
-        
-        element = WebDriverWait(self.browser, delay).until(EC.visibility_of_element_located(locator))
-        return element
+
+        try:
+            element = WebDriverWait(
+                self.browser, delay
+            ).until(EC.visibility_of_element_located((locator)))
+            return element
+        except TimeoutException, e:
+            self.logger.debug(
+                "Timed out waiting for element '%s' to display." % locator[1])
+            return None
+        except NoSuchElementException, e:
+            self.logger.debug("Element '%s' was never found." % locator[1])
+            return None
+        except Exception, e:
+            self.logger.debug("Failed to locate element. ERROR: %s" % str(e))
+            return None
 
     def ajax_complete(self, driver):
         try:
